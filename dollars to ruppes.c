e43dfd81409621ce64582cdf4f99fba539a0e1fc
dhruvparmar2707 <dhruvparmar149@gmail.com>
2025-08-29 00:26:03 +0530

    Add files via upload

---
+++ dollars to ruppes.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+int dollars,ruppes;
+printf("enter the dollars");
+scanf("%d",&dollars);
+
+ruppes=dollars*48;
+
+printf("multi=%d",ruppes);
+return 0;
+}
---
+++ ruppes to dollar.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+float main()
+{
+float dollars,ruppes;
+printf("enter the ruppes");
+scanf("%f",&ruppes);
+
+dollars=ruppes/48;
+
+printf("division=%f",dollars);
+return 0;
+}
