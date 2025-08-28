f1d9c6fcde02b0c6b41138a59e1e71c2e0f9947b
dhruvparmar2707 <dhruvparmar149@gmail.com>
2025-08-28 15:04:28 +0530

    Add files via upload

---
+++ 24bee022(01).py
@@ -0,0 +1,17 @@
+#def for_loop_example():
+    #n=int(input["Enter number:"])
+    #for i in range (0,n);
+    #print(i)
+    #for_loop_example()
+
+
+def counting_vowels():
+    n=int(input("Enter a name"))
+    vowels="aeiou"
+    i=0
+    for char in string:
+        if char is vowels:
+            i+=1
+            return i
+        number vowels=counting_vowels(n)
+        print("Number of vowels are as follows:",numbers_vowels)
