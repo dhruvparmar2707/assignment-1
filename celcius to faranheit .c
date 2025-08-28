93f47ce42a9db3e982cebd314df3da66237b7e1f
dhruvparmar2707 <dhruvparmar149@gmail.com>
2025-08-29 00:45:26 +0530

    Add files via upload

---
+++ celcius to faranheit .c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+float main()
+{
+float celsius,faranheit;
+printf("enter the celsius");
+scanf("%f",&celsius);
+
+faranheit=9/5*celsius+32;
+printf("all operations=%f",faranheit);
+return 0;
+}
