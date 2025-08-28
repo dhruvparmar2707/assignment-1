636046d656143af3ba0306cb8407f5333dfa9f08
dhruvparmar2707 <dhruvparmar149@gmail.com>
2025-08-28 23:37:32 +0530

    Add files via upload

---
+++ division.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+float main()
+{
+float i,j,k;
+printf("enter first number");
+scanf("%f",&i);
+printf("enter second number");
+scanf("%f",&j);
+k=i/j;
+printf("division=%f",k);
+return 0;
+}
