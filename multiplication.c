d794c814600cd19a09021af530d4cebc140c579b
dhruvparmar2707 <dhruvparmar149@gmail.com>
2025-08-28 23:36:10 +0530

    Add files via upload

---
+++ multiplication.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+int i,j,k;
+printf("enter first number");
+scanf("%d",&i);
+printf("enter second number");
+scanf("%d",&j);
+k=i*j;
+printf("multi=%d",k);
+return 0;
+}
