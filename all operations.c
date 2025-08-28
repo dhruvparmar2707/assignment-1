593f5ae29e3fe5b54ec12b3010a3131987296c63
dhruvparmar2707 <dhruvparmar149@gmail.com>
2025-08-28 23:47:28 +0530

    Add files via upload

---
+++ all operations.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+float main()
+{
+float i,j,k,l,m,n;
+printf("enter first number");
+scanf("%f",&i);
+printf("enter second number");
+scanf("%f",&j);
+printf("enter third number");
+scanf("%f",&k);
+printf("enter fourth number");
+scanf("%f",&l);
+printf("enter fifth number");
+scanf("%f",&m);
+n=i+j-k*l/m;
+
+printf("all operations=%f",n);
+return 0;
+}
