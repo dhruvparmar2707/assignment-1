c7e43c2729984331dade0870f17693681c335cbd
dhruvparmar2707 <dhruvparmar149@gmail.com>
2025-08-29 22:33:36 +0530

    Add files via upload

---
+++ fahrenheit into celsius .c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+
+
+
+float main() {
+    float c,f;
+
+
+    printf("the value of temperature fahrenheit X= \n" );
+    scanf("%f",&f);
+
+    c=(0.56)*(f-32);
+    printf("CELSIUS=%f", c);
+
+    return 0;
+}
+
