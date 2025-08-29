39d35b0146d802771783a028d777bfba3283246d
dhruvparmar2707 <dhruvparmar149@gmail.com>
2025-08-29 11:30:49 +0530

    Add files via upload

---
+++ swapping .c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+float main()
+{
+float a,b,temp;
+printf("enter the a");
+scanf("%f",&a);
+printf("enter the b");
+scanf("%f",&b);
+
+
+temp=a;
+a=b;
+b=temp;
+
+
+printf("after swapping:/n");
+printf("a=%f\n",a);
+printf("b=%f\n",b);
+return 0;
+}
