c01e89d82c383ce75bba792b61566a02883ec886
dhruvparmar2707 <dhruvparmar149@gmail.com>
2025-08-29 09:29:41 +0530

    Add files via upload

---
+++ interest.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+float main()
+{
+float P,R,T,interest;
+printf("enter the P");
+scanf("%f",&P);
+printf("enter the R");
+scanf("%f",&R);
+printf("enter the T");
+scanf("%f",&T);
+interest=(P*R*T)/100;
+
+printf("interest=%f",interest);
+return 0;
+}
