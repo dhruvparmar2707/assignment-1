f3dbf076ecbe4e69062d848995d26a5e25b7315e
dhruvparmar2707 <dhruvparmar149@gmail.com>
2025-08-29 11:52:11 +0530

    Add files via upload

---
+++ grams to kilos.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+float main()
+{
+float grams,kilos;
+printf("enter the grams");
+scanf("%f",&grams);
+
+kilos=grams/1000;
+
+printf("division=%f",kilos);
+return 0;
+}
---
+++ kilos to grams.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+int grams,kilos;
+printf("enter the kilos");
+scanf("%d",&kilos);
+
+grams=kilos*1000;
+
+printf("multi=%d",grams);
+return 0;
+}
